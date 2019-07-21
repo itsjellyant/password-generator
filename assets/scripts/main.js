15ebe16febe86a9b69f162001a429ca3732df115
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-07-21 02:00:38 -0500

    finished the recusion and did the html and set up the css

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,31 @@
-const max = 20
+const length = 20
 const numbers = Array.from("123456789");
 const upperCaseAlphabets = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 const lowerCaseAlphabets = Array.from("abcdefghijklmnopqrstuvwxyz");
+const symbols = Array.from("")
 
 const button = document.querySelector("#password");
-const input = document.querySelector("#input");
+const input = document.querySelector("#password-input");
 
 button.addEventListener("click", passwordGenerator);
 
 function passwordGenerator() {
     let password = [];
     let allCharacters = numbers.concat(upperCaseAlphabets, lowerCaseAlphabets);
-    for(let i = 0; i < max; i++) {
+    let uniquePassword = function(array) {
         let random = Math.floor(Math.random() * allCharacters.length);
-        let characters = allCharacters[random]
-        password.push(characters);
+        let characters = allCharacters[random];
+        if (array.length > length) {
+            return;
+        } else {
+            password.push(characters);
+            uniquePassword(password);
+        }
     }
-        return input.setAttribute("value", `${password.slice(0, max).join("")}`);
+    uniquePassword(password);
+    return input.setAttribute("value", `${password.slice(0, length).join("")}`);
 }
+
+
+
 
