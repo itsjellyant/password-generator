718e54ba4e6e983f48061b8d7b9933b8d8d63310
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-07-21 23:21:42 -0500

    Added regex and variables

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,32 @@
+/*
+Table of Contents
+    1. Variables
+        - Password Generator - iife
+        - Regular Expressions - match
 
+*/
+
+/***********************************
+        Password Generator - iife
+************************************/
+
+(function() {
+
+    let password = "";
+    let numbers = "0123456789";
+    let symbols = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    let uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowercaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
+
+
+}());
+
+/***********************************
+    Regular Expressions - match
+************************************/
+
+const NUMBERS_REGEX = /([\d])/g;
+const SYBMOLS_REGEX =  /([\?\-])/g;
+const LOWERCASE_ALPHABETS_REGEX = /([a-z])/g;
+const UPPERCASE_ALPHABETS_REGEX= /([A-Z])/g;
 
