9b835a3cd0216a40fb7f2b7f99da8183f77d371f
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-09-15 00:32:28 -0500

    rebuilding a new design

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,26 +7,23 @@ Table of Contents
 */
 
 /***********************************
-        Password Generator - iife
+        Password Generator 
 ************************************/
+class {
+    constructor(generatedPassword) {
+        this.generatedPassword = generatedPassword;
+    }
 
-(function() {
-
-    let password = "";
-    let numbers = "0123456789";
-    let symbols = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-    let uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let lowercaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
-
-
-}());
+    
+}
 
 /***********************************
     Regular Expressions - match
 ************************************/
-
-const NUMBERS_REGEX = /([\d])/g;
-const SYBMOLS_REGEX =  /([\?\-])/g;
-const LOWERCASE_ALPHABETS_REGEX = /([a-z])/g;
-const UPPERCASE_ALPHABETS_REGEX= /([A-Z])/g;
+const password = {
+    bad: /[/d]/,
+    weak: /[\w]/,
+    medium: /^[\w\d]$/,
+    high: /^[]$/,
+}
 
